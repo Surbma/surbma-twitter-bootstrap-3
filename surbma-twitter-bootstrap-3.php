6d6e3c59ee3eaa18c860495d028c9309b4ae2443
Peter Ambrus <peter@surbma.hu>
2014-01-22 14:52:42 +0100

    1.3.0 - Bootstrap 3.0.3
    - Updated to Bootstrap 3.0.3
    - Removed Glyphicons
    - Removed Badges
    - Removed Progress bars
    - Fixed all WordPress related bugs in css
    - Added $allowedposttags for buttons

--- surbma-twitter-bootstrap-3.php
+++ surbma-twitter-bootstrap-3.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma - Twitter Bootstrap 3
 Plugin URI: http://surbma.com/
 GitHub Plugin URI: Surbma/surbma-twitter-bootstrap-3
 Description: Twitter Bootstrap 3
-Version: 1.2.0
+Version: 1.3.0
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
@@ -24,8 +24,8 @@ function surbma_twitter_bootstrap_scripts() {
 add_action( 'wp_footer', 'surbma_twitter_bootstrap_scripts' );
 
 function surbma_twitter_bootstrap_enqueue_scripts() {
-	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array( 'jquery' ), '3.0.0', true );
-	wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '3.0.0' );
+	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array( 'jquery' ), '3.0.3', true );
+	wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '3.0.3' );
 }
 add_action( 'wp_enqueue_scripts', 'surbma_twitter_bootstrap_enqueue_scripts' );
 
@@ -54,3 +54,26 @@ $allowedposttags['a'] = array(
 	'data-parent' => array(),
 	'data-dismiss' => array()
 );
+$allowedposttags['button'] = array(
+	'disabled' => array(),
+	'name' => array(),
+	'type' => array(),
+	'value' => array(),
+	'class' => array(),
+	'id' => array(),
+	'style' => array(),
+	'data-animation' => array(),
+	'data-html' => array(),
+	'data-placement' => array(),
+	'data-selector' => array(),
+	'data-trigger' => array(),
+	'data-title' => array(),
+	'data-delay' => array(),
+	'data-container' => array(),
+	'data-toggle' => array(),
+	'data-target' => array(),
+	'data-content' => array(),
+	'data-parent' => array(),
+	'data-dismiss' => array()
+);
+
