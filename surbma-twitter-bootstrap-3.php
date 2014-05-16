ad1a72563b571ea19646b1b9783dec2257d0f666
Surbma <peter@surbma.hu>
2014-05-17 01:46:52 +0200

    1.4.1
    - Fixed button color display for every case

--- surbma-twitter-bootstrap-3.php
+++ surbma-twitter-bootstrap-3.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma - Twitter Bootstrap 3
 Plugin URI: http://surbma.com/
 GitHub Plugin URI: Surbma/surbma-twitter-bootstrap-3
 Description: Twitter Bootstrap 3
-Version: 1.4.0
+Version: 1.4.1
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
